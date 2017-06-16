3738ccdc655699b24b95dfa25f984709918dee2f
Eric Milaneze <eric@milaneze.com.br>
2017-06-16 01:07:01 -0300

    Ajustes de margem.

--- src/app/cantar/cantar.component.ts
+++ src/app/cantar/cantar.component.ts
@@ -15,7 +15,7 @@ export class CantarComponent implements OnInit {
   divContainer: ElementRef;
 
   private defaults = {
-    tempoInicial: 10,
+    tempoInicial: 2,
     ratioLargura: 16,
     ratioAltura: 9,
     alturaMenuMargin: 75,
